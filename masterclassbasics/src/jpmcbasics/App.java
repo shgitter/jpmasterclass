0f670a1f962d2af414060a059373e7b0a218fa61
shgitter <serge860226@gmail.com>
2019-06-26 13:09:45 +0400

    all my files.

---
+++ masterclassbasics/src/jpmcbasics/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jpmcbasics;
+
+/**
+ * @author sergeyhu
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
